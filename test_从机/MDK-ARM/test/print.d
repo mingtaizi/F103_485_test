e9d9e83ec214a2711747780b7cf96033a4764a44
mingtaizi <todobest@Outlook.com>
2019-07-22 23:06:07 +0800

    说明：103开发板上的485通信代码，工作流程：^C机先向从机发送一帧 数据，从机接收到数据后回一帧，主机接收到回的数据后再向从机发，数据从0001开始加，每次接收到数据都会加1。实现方式：通过 dma + usart 实现，可以在串口中断里通过IDLE标志位接收不定长数据。延迟大概10ms一帧，因为485的接收转发送延时5ms，发送转接收延时5ms。

---
+++ test_从机/MDK-ARM/test/print.d
@@ -0,0 +1,26 @@
+test\print.o: print.c
+test\print.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+test\print.o: ../Inc/stm32f1xx_hal_conf.h
+test\print.o: ../Inc/main.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+test\print.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+test\print.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+test\print.o: ../Drivers/CMSIS/Include/core_cm3.h
+test\print.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+test\print.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+test\print.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+test\print.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+test\print.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+test\print.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
